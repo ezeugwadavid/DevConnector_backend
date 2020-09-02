2b4f09e407e49693938e2f1efc9918e02cda2310
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2020-09-02 13:06:18 +0100

    save changes upto creating user token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://david:66139868AH@david.nlatp.mongodb.net/mern-stack'
+    mongoURI: 'mongodb+srv://david:66139868AH@david.nlatp.mongodb.net/mern-stack',
+    secretOrKey: 'secret'
 };
