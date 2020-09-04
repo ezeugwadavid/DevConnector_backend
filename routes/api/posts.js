3a7fe584afbd442b6b399e08b147ae0bbb59dc6e
Gerrard Ezeugwa <ebubeezeugwa@gmail.com>
2020-09-04 23:45:16 +0100

    commit all and final post routes and backend

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -171,8 +171,19 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
 //@access Private
 
 router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+
+    // Check Validation
+
+    if (!isValid) {
+        // If any errors, send 400 with errors object
+        return res.status(400).json(errors);
+
+    }
+
     Post.findById(req.params.id)
     .then(post => {
+        
         const newComment = {
             text: req.body.text,
             name: req.body.name,
@@ -180,8 +191,10 @@ router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (r
             user: req.user.id
 
         }
+       
 
         // Add to comments array
+        
         post.comments.unshift(newComment);
 
         // Save
@@ -189,6 +202,41 @@ router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (r
 
     })
     .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    
+
+});
+
+
+// @route DELETE api/posts/comment/:id/:comment_id
+//@desc remove comment from post
+//@access Private
+
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+    Post.findById(req.params.id)
+    .then(post => {
+        // Check to see if comment exists
+        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length
+        === 0) {
+        return res.status(404).json({ postnotfound: 'no post found' })
+        }
+
+        //Get remove index
+
+        const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+        // Splice comment out of array
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+
+        
+
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    
 
 });
 
