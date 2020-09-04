493b87011d832cdf761d4148829691ea55441ab1
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2020-09-04 14:49:31 +0100

    commit to all profile and users route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const app = express();
+const passport = require('passport');
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,10 +17,18 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db).then(() => console.log('MongoDB Connected'))
+mongoose.connect(db,{ useNewUrlParser: true,  useUnifiedTopology: true }).then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+// passport middleware
+
+app.use(passport.initialize());
+
+// Passport Config
+
+require('./config/passport')(passport);
+
+
 
 // Use Routes
 
