2a7046eb7c8dd507d090978f818f78d04a2fadf0
Gerrard Ezeugwa <ebubeezeugwa@gmail.com>
2020-09-09 21:27:30 +0100

    till profile state

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,39 @@ import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import './App.css';
 
 // Check for token 
-if(localStorage) {
+if(localStorage.jwtToken) {
   // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
   // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   //Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    //  Clear Current Profile
+
+    store.dispatch(clearCurrentProfile())
+
+    // Redirect to login
+    window.location.href = '/login';
+
+  }
 }
 
 function App() {
@@ -33,6 +50,7 @@ function App() {
           <div className="container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/dashboard" component={Dashboard} />
           </div>
 
           <Footer />
